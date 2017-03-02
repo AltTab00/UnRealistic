9e93e0051dfb2e14ac1dd00d83dc0a9d7deba52e
AltTab00 <aalt@sapo.pt>
2017-03-02 05:27:51 +0000

    Create recipe.lua

---
+++ prototypes/recipe/recipe.lua
@@ -0,0 +1,15 @@
+data:extend({
+{
+  {
+    type = "recipe",
+    name = "lab-0",
+    enabled = "true",
+    energy_required = 15,
+    ingredients =
+    {
+      {"raw-wood", 20}
+    },
+    result = "lab-0"
+  },
+}
+)
